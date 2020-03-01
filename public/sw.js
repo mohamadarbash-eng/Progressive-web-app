eea7d4d363a16bcbbcda52cee8cec7de05c7b052
Arbash Mohamad <eng@mohamadarbash.com>
2020-03-01 14:54:47 +0100

    add eventlistener in serviceworker

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,14 @@
+self.addEventListener("install", function (event) {
+    console.log("install", event);
+});
+
+self.addEventListener("activate", function (event) {
+    console.log("activated", event);
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+    console.log("fetch", event);
+    event.respondWith(fetch(event.request));
+});
 
