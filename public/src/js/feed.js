96dd40d39e7fa2722b768df352e7471be3bff461
Arbash Mohamad <eng@mohamadarbash.com>
2020-03-15 01:11:48 +0100

    cache on demand

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -18,6 +18,17 @@ function closeCreatePostModal() {
   createPostArea.style.display = 'none';
 }
 
+function onSaveClicked () {
+  console.log('clicked');
+  if ('caches' in window) {
+    caches.open('user-request')
+        .then((cache) => {
+          cache.add('https://httpbin.org/get');
+          cache.add('/src/images/sf-boat.jpg');
+        });
+  }
+}
+
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
@@ -41,6 +52,10 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.addEventListener('click', onSaveClicked)
+  cardSaveButton.textContent = 'save';
+  cardSupportingText.appendChild(cardSaveButton);
   cardWrapper.appendChild(cardSupportingText);
   componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
--- public/sw.js
+++ public/sw.js
@@ -51,7 +51,7 @@ self.addEventListener('fetch', (event) => {
                         .then((res) => {
                             return caches.open(dynamicName)
                                 .then( (cache) => {
-                                    cache.put(event.request.url, res.clone());
+                                  //  cache.put(event.request.url, res.clone());
                                     return res;
                                 })
                         }).catch();
